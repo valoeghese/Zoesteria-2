7333404f70cd346203b019f283e6e16f4d82f012
valoeghese <valoeghesemods@gmail.com>
2020-12-11 11:58:40 +1300

    enough for now

---
+++ src/main/java/tk/valoeghese/zoesteria/common/feature/FallenLogFeature.java
@@ -0,0 +1,7 @@
+package tk.valoeghese.zoesteria.common.feature;
+
+import net.minecraft.world.gen.feature.Feature;
+
+public class FallenLogFeature extends Feature<TreeLikeFeatureConfig> {
+
+}
---
+++ src/main/java/tk/valoeghese/zoesteria/common/feature/TreeLikeFeatureConfig.java
@@ -0,0 +1,18 @@
+package tk.valoeghese.zoesteria.common.feature;
+
+import com.mojang.datafixers.Dynamic;
+import com.mojang.datafixers.types.DynamicOps;
+
+import net.minecraft.world.gen.blockstateprovider.BlockStateProvider;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+
+public class TreeLikeFeatureConfig implements IFeatureConfig {
+	public TreeLikeFeatureConfig(BlockStateProvider log, int baseSize, int sizeRandom) {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public <T> Dynamic<T> serialize(DynamicOps<T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
