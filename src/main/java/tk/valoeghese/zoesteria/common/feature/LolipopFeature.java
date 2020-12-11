a3a803463a19cd0237e7aef8b8c0c9bfc44be5a8
valoeghese <valoeghesemods@gmail.com>
2020-12-11 19:12:50 +1300

    progress

--- src/main/java/tk/valoeghese/zoesteria/common/feature/LolipopFeature.java
+++ src/main/java/tk/valoeghese/zoesteria/common/feature/LolipopFeature.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.Set;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.Mutable;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.world.gen.IWorldGenerationReader;
 import net.minecraft.world.gen.feature.AbstractTreeFeature;
@@ -36,12 +37,67 @@ public class LolipopFeature extends AbstractTreeFeature<TreeFeatureConfig> {
 			}
 		}
 
-		int foliageDepth = config.foliageHeight + rand.nextInt(config.foliageHeightRandom);
+		// trunk height thus indicates the height of the bare trunk, and the rest is foliage-covered
+		final int trunkHeight = height - (config.trunkHeight + rand.nextInt(config.trunkHeightRandom + 1));
+
+		// trunk top offset in the config thus indicates the amount of bare foliage at the end
+		final int foliageDepth = height - (config.trunkTopOffset + rand.nextInt(config.trunkTopOffsetRandom + 1));
 
 		pos.setPos(start);
 
 		if (isSoil(world, start.down(), null)) {
-			
+			// this code was adapted from an old mod I wrote last year
+			for (int yOff = 0; yOff <= height; ++yOff) {
+				pos.setY(startY + yOff);
+
+				if (yOff == height) {
+					this.setLeaf(world, rand, pos, leaves, box, config);
+				} else if (yOff == height - 1 || yOff == 0) {
+					this.plusShape(world, rand, pos, leaves, box, startX, startZ, config);
+				} else {
+					for (int xOff = -1; xOff < 2; ++xOff) {
+						pos.setX(startX + xOff);
+
+						for (int zOff = -1; zOff < 2; ++zOff) {
+							pos.setZ(startZ + zOff);
+
+							if(xOff != 0 && zOff !=0) {
+								this.setLeaf(world, rand, pos, leaves, box, config);
+							}
+						}
+					}
+
+					if (yOff == 1 || yOff == height - 2) {
+						generator.setBlock(origin.add(0, yOff, -2), LEAVES, true); // It happened here once
+						// appears to crash either with leaves or at a certain distance away? :thonkjang:
+						generator.setBlock(origin.add(0, yOff, 2), LEAVES, true);
+						generator.setBlock(origin.add(-2, yOff, 0), LEAVES, true);
+						generator.setBlock(origin.add(2, yOff, 0), LEAVES, true);
+					}
+				}
+			}
+
+			for (int i = 0; i < height; ++i) {
+				generator.setBlock(pos.add(0, i, 0), LOG, false);
+			}
+
+			return true;
 		}
+
+		return false;
+	}
+
+	private void plusShape(IWorldGenerationReader world, Random rand, Mutable pos, Set<BlockPos> leaves, MutableBoundingBox box, int startX, int startZ, int offset, TreeFeatureConfig config) {
+		pos.setZ(startZ - offset);
+		this.setLeaf(world, rand, pos, leaves, box, config);
+		pos.setZ(startZ + offset);
+		this.setLeaf(world, rand, pos, leaves, box, config);
+		pos.setZ(startZ);
+
+		pos.setX(startX - offset);
+		this.setLeaf(world, rand, pos, leaves, box, config);
+		pos.setX(startX - offset);
+		this.setLeaf(world, rand, pos, leaves, box, config);
+		pos.setX(startX);
 	}
 }
