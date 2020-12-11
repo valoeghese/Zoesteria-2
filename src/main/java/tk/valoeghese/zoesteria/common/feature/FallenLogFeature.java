615f45b890b856b5b1542e54b0ffeb983e010335
valoeghese <valoeghesemods@gmail.com>
2020-12-11 16:07:38 +1300

    feature serialiser for tree like

--- src/main/java/tk/valoeghese/zoesteria/common/feature/FallenLogFeature.java
+++ src/main/java/tk/valoeghese/zoesteria/common/feature/FallenLogFeature.java
@@ -1,7 +1,22 @@
 package tk.valoeghese.zoesteria.common.feature;
 
+import java.util.Random;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationSettings;
 import net.minecraft.world.gen.feature.Feature;
 
 public class FallenLogFeature extends Feature<TreeLikeFeatureConfig> {
+	public FallenLogFeature() {
+		super(TreeLikeFeatureConfig::deserialize);
+	}
 
+	@Override
+	public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> generator, Random rand, BlockPos pos, TreeLikeFeatureConfig config) {
+		int size = config.baseSize + rand.nextInt(config.sizeRandom + 1);
+		
+		return false;
+	}
 }
---
+++ src/main/java/tk/valoeghese/zoesteria/common/feature/TreeLikeFeatureConfigSerialiser.java
@@ -0,0 +1,45 @@
+package tk.valoeghese.zoesteria.common.feature;
+
+import net.minecraft.world.gen.blockstateprovider.BlockStateProvider;
+import tk.valoeghese.zoesteria.api.feature.IFeatureConfigSerialiser;
+import tk.valoeghese.zoesteria.core.serialisers.BlockStateProviderHandler;
+import tk.valoeghese.zoesteriaconfig.api.container.Container;
+import tk.valoeghese.zoesteriaconfig.api.container.EditableContainer;
+
+public class TreeLikeFeatureConfigSerialiser implements IFeatureConfigSerialiser<TreeLikeFeatureConfig> {
+	private TreeLikeFeatureConfigSerialiser(BlockStateProvider log, int minSize, int maxSize) {
+		this.log = log;
+		this.minSize = minSize;
+		this.maxSize = maxSize;
+	}
+
+	private final BlockStateProvider log;
+	private final int minSize;
+	private final int maxSize;
+
+	@Override
+	public IFeatureConfigSerialiser<TreeLikeFeatureConfig> loadFrom(TreeLikeFeatureConfig config) {
+		final int minSize = config.baseSize;
+		return new TreeLikeFeatureConfigSerialiser(config.logProvider, minSize, minSize + config.sizeRandom);
+	}
+
+	@Override
+	public IFeatureConfigSerialiser<TreeLikeFeatureConfig> deserialise(Container settings) {
+		return new TreeLikeFeatureConfigSerialiser(
+				BlockStateProviderHandler.stateProvider(settings.getContainer("log")),
+				settings.getIntegerValue("minSize"),
+				settings.getIntegerValue("maxSize"));
+	}
+
+	@Override
+	public void serialise(EditableContainer settings) {
+		BlockStateProviderHandler.serialiseStateProvider(this.log);
+		settings.putIntegerValue("minSize", this.minSize);
+		settings.putIntegerValue("maxSize", this.maxSize);
+	}
+
+	@Override
+	public TreeLikeFeatureConfig create() {
+		return new TreeLikeFeatureConfig(this.log, this.minSize, this.maxSize - this.minSize);
+	}
+}
