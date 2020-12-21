dfcef7f4c6ed40fa1ca8003c5a2117ae98efbf20
valoeghese <valoeghesemods@gmail.com>
2020-12-21 20:35:00 +1300

    outback plateau surface builder

--- src/main/java/tk/valoeghese/zoesteria/common/biome/AustralianOutback.java
+++ src/main/java/tk/valoeghese/zoesteria/common/biome/AustralianOutback.java
@@ -35,12 +35,14 @@ public class AustralianOutback implements IZoesteriaBiome {
 				.depth(0.3f)
 				.scale(0.11f)
 				.temperature(1.6f)
-				.downfall(0.1f);
+				.downfall(0.1f)
+				.fillerBlock("minecraft:red_sandstone")
+				.underwaterBlock("minecraft:red_sand");
 
 		if (this.plateau) {
-			// TODO
+			builder.surfaceBuilder("zoesteria:fill_to_sea_level").topBlock("minecraft:red_sandstone");
 		} else {
-			builder.surfaceBuilder("zoesteria:outback").topBlock("minecraft:red_sand").fillerBlock("minecraft:red_sandstone").underwaterBlock("minecraft:red_sand");
+			builder.surfaceBuilder("zoesteria:outback").topBlock("minecraft:red_sand");
 		}
 
 		return builder.build();
