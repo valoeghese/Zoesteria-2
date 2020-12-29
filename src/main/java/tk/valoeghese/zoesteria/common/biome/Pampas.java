6ed9a72383cc332aeb085ef4518f199177d24b2c
valoeghese <valoeghesemods@gmail.com>
2020-12-29 16:29:33 +1300

    start pampas grasslands

--- src/main/java/tk/valoeghese/zoesteria/common/biome/Pampas.java
+++ src/main/java/tk/valoeghese/zoesteria/common/biome/Pampas.java
@@ -1,9 +1,10 @@
 package tk.valoeghese.zoesteria.common.biome;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import net.minecraftforge.common.BiomeDictionary.Type;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.BiomeManager.BiomeType;
 import tk.valoeghese.zoesteria.api.biome.BiomeDecorations;
 import tk.valoeghese.zoesteria.api.biome.IBiome;
@@ -11,10 +12,17 @@ import tk.valoeghese.zoesteria.api.biome.IBiomeProperties;
 import tk.valoeghese.zoesteria.api.biome.SpawnEntry;
 
 public class Pampas implements IBiome {
+	public Pampas(String id, Type type) {
+		this.id = id;
+		this.type = type;
+	}
+
+	private final String id;
+	private final Type type;
+
 	@Override
 	public String id() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.id;
 	}
 
 	@Override
@@ -41,14 +49,29 @@ public class Pampas implements IBiome {
 	}
 
 	@Override
-	public List<Type> biomeTypes() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<BiomeDictionary.Type> biomeTypes() {
+		List<BiomeDictionary.Type> result = new ArrayList<>();
+		result.add(BiomeDictionary.Type.OVERWORLD);
+		result.add(BiomeDictionary.Type.PLAINS);
+
+		if (this.type == Type.FLATS) {
+			result.add(BiomeDictionary.Type.SPARSE);
+		} else if (this.type == Type.HILLS) {
+			result.add(BiomeDictionary.Type.HILLS);
+		}
+
+		return result;
 	}
 
 	@Override
 	public List<SpawnEntry> mobSpawns() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public enum Type {
+		FLATS,
+		NORMAL,
+		HILLS
 	}
 }
