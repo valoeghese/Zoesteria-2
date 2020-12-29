20bf6ad892a09a8717b68a72772fe6edaaab9c77
valoeghese <valoeghesemods@gmail.com>
2020-12-29 16:43:46 +1300

    a few basic properties of pampas

--- src/main/java/tk/valoeghese/zoesteria/common/biome/Pampas.java
+++ src/main/java/tk/valoeghese/zoesteria/common/biome/Pampas.java
@@ -3,13 +3,34 @@ package tk.valoeghese.zoesteria.common.biome;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.GenerationStage.Decoration;
+import net.minecraft.world.gen.blockplacer.DoublePlantBlockPlacer;
+import net.minecraft.world.gen.blockplacer.SimpleBlockPlacer;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.feature.BlockBlobConfig;
+import net.minecraft.world.gen.feature.BlockClusterFeatureConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.structure.VillageConfig;
+import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
+import net.minecraft.world.gen.placement.FrequencyConfig;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.TopSolidWithNoiseConfig;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.BiomeManager.BiomeType;
 import tk.valoeghese.zoesteria.api.biome.BiomeDecorations;
+import tk.valoeghese.zoesteria.api.biome.BiomeDefaultFeatures;
 import tk.valoeghese.zoesteria.api.biome.IBiome;
 import tk.valoeghese.zoesteria.api.biome.IBiomeProperties;
 import tk.valoeghese.zoesteria.api.biome.SpawnEntry;
+import tk.valoeghese.zoesteria.common.ZoesteriaCommonEventHandler;
+import tk.valoeghese.zoesteria.common.objects.ZoesteriaBlocks;
 
 public class Pampas implements IBiome {
 	public Pampas(String id, Type type) {
@@ -44,8 +65,29 @@ public class Pampas implements IBiome {
 
 	@Override
 	public BiomeDecorations getDecorations() {
-		// TODO Auto-generated method stub
-		return null;
+		BiomeDecorations decorations = BiomeDecorations.create()
+				.addDecoration(Decoration.LOCAL_MODIFICATIONS, Feature.FOREST_ROCK
+						.withConfiguration(new BlockBlobConfig(Blocks.STONE.getDefaultState(), 1))
+						.withPlacement(Placement.TOP_SOLID_HEIGHTMAP_NOISE_BIASED.configure(new TopSolidWithNoiseConfig(
+								9, // amplitude
+								32.0, // stretch of coordinates (pos / val)
+								-0.5, // offset of raw noise
+								Heightmap.Type.OCEAN_FLOOR_WG))))
+				.addDecoration(Decoration.VEGETAL_DECORATION, ZoesteriaCommonEventHandler.CONFIGURED_HICKORY
+						.withPlacement(Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(0, 0.03f, 9))));
+
+		BiomeDefaultFeatures.addGrass(decorations, 40);
+		BiomeDefaultFeatures.addWaterLakes(decorations, Blocks.WATER.getDefaultState(), 50);
+		BiomeDefaultFeatures.addLavaLakes(decorations, Blocks.LAVA.getDefaultState(), 95);
+		BiomeDefaultFeatures.addOres(decorations);
+		BiomeDefaultFeatures.addSedimentDisks(decorations);
+		BiomeDefaultFeatures.addStoneVariants(decorations);
+		BiomeDefaultFeatures.addMushrooms(decorations, 2, 1);
+
+		decorations.addStructure(Feature.PILLAGER_OUTPOST.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
+		decorations.addStructure(Feature.VILLAGE.withConfiguration(new VillageConfig("village/plains/town_centers", 4)));
+
+		return decorations;
 	}
 
 	@Override
@@ -65,8 +107,23 @@ public class Pampas implements IBiome {
 
 	@Override
 	public List<SpawnEntry> mobSpawns() {
-		// TODO Auto-generated method stub
-		return null;
+		return ImmutableList.of(
+				new SpawnEntry(EntityType.COW).spawnWeight(14).spawnGroupCount(3, 5),
+				new SpawnEntry(EntityType.SHEEP).spawnWeight(14).spawnGroupCount(3, 5),
+				new SpawnEntry(EntityType.RABBIT).spawnWeight(8).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.CHICKEN).spawnWeight(5).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.PIG).spawnWeight(2).spawnGroupCount(2, 3),
+
+				new SpawnEntry(EntityType.BAT).spawnWeight(10).spawnGroupCount(8, 8),
+
+				new SpawnEntry(EntityType.SPIDER).spawnWeight(100).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.ZOMBIE).spawnWeight(95).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.ZOMBIE_VILLAGER).spawnWeight(5).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.SKELETON).spawnWeight(100).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.CREEPER).spawnWeight(100).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.SLIME).spawnWeight(100).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.ENDERMAN).spawnWeight(10).spawnGroupCount(2, 4),
+				new SpawnEntry(EntityType.WITCH).spawnWeight(5).spawnGroupCount(2, 4));
 	}
 
 	public enum Type {
